22e59df9ee328439ed59682c1757c0279767ff8a
mario hernandez <maarioherns21@gmail.com>
2022-09-16 14:19:39 +0800

    fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
-import Button from "../../components/Button/Button"
 import Movies from "../../components/Movies/Movies"
-import NewMovies from "../../components/NewMovies/NewMovies"
+
 
 
 export default function Home (){
