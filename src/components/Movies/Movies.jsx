a758ac60f6a0208fa431376d78e618dbb423a0cb
mario hernandez <maarioherns21@gmail.com>
2022-09-17 23:29:21 +0800

    add search prop

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,8 +1,10 @@
 //this is the main movies 
 
+import { useEffect } from "react";
 import { useState } from "react";
 import MovieList from "../MovieList/MovieList";
 import Pagination from "../Pagination/Pagination.";
+import SearchBar from "../SearchBar/SearchBar";
 import useFetch from "../useFetch/useFetch";
 
 
@@ -11,20 +13,29 @@ export default function Movies() {
 const {movies, error, isLoading, handleDelete} = useFetch("http://localhost:8000/movies")
 const [currentPage, setCurrentPage] =useState(1)
 const [moviesPerPage,setMoviesPerPage ] =useState(2)
-
+// const [input, setInPut]= useState("")
+// const [output, setOutPut] = useState([])
 
 const lastMovieIndex =  currentPage * moviesPerPage;
 const firstMovieIndex = lastMovieIndex - moviesPerPage;
 const currentMovies = movies.slice(firstMovieIndex , lastMovieIndex)
 
 
-
+// useEffect(() =>{
+//     setOutPut([])
+// movies.filter((movie) =>{
+//     if(movie.name.toLowerCase().includes(input.toLowerCase())){
+//         setOutPut(output=> [...output, movie])
+//     }
+// })
+// }, [input])
 
 
 return (
 <div>
     {error && <div>{error}</div>}
     {isLoading && <div>Loading...</div>}
+  {movies &&  <SearchBar movies={movies} handleDelete={handleDelete} />}
 {movies && <MovieList movies={currentMovies} title="All Movies"  handleDelete={handleDelete} />}
 <Pagination totalMovies={movies.length} moviesPerPage={moviesPerPage} setCurrentPage={setCurrentPage} setPostPerPage={setMoviesPerPage} />
 </div>
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Popup from "reactjs-popup";
+import "./SearchBar.css";
+    ///this components alllows to cretate a new arr of movies
+//this is a prop component  
+
+// this is  props  compoents 
+export default function SearchBar ({movies, handleDelete}) {
+  const [input, setInPut] = useState(movies);
+  const [output, setOutPut] = useState([]);
+
+  useEffect(() => {
+    setOutPut([]);
+    movies.filter((movie) => {
+      if (movie.name.toLowerCase().includes(input.toLowerCase())) {
+        setOutPut((output) => [...output, movie]);
+
+      }
+    })
+  }, [input]);
+
+console.log(output)
+
+  return(
+    <div>
+      <div className="search-header">
+        <div className="search-text">Search:</div>
+        <input id="search-box" onChange={(e) => setInPut(e.target.value)} />
+      </div>
+      <Popup className="form modal" trigger={<button>search</button>}>
+    {output.map((movie) => (
+        <div key={movie.id}>
+            <h2>{movie.name}</h2>
+            <Link to={`/movie/${movie.id}`}>
+             <img src={movie.images} alt={movie.name} />
+            <h3>{movie.body}</h3>
+            </Link>
+            <button onClick={() => handleDelete(movie.id)}>delete</button>
+        </div>
+    ))}
+      </Popup>
+    
+</div>
+  )
+}
