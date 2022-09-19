63a6134a23a40c1fc9c028b25d29e0341eafbe4e
mario hernandez <maarioherns21@gmail.com>
2022-09-19 16:39:54 +0800

    Adding  prevent method

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -4,59 +4,44 @@ import "./Form.css";
 
 
 
-export default function EditForm({movie}) {
-    const [name, setName] = useState(movie.name);
-    const [body, setBody] = useState(movie.body);
-    const [creator, setCreator] = useState(movie.creator);
-    const [isPending, setIsPending] = useState(false);
-    const [images, setImages] = useState(movie.images);
-    const navigate = useNavigate();
-    const PUT = "PUT";
-    const { id } = useParams();
+export default function EditForm({ movie }) {
+  const [name, setName] = useState(movie.name);
+  const [body, setBody] = useState(movie.body);
+  const [creator, setCreator] = useState(movie.creator);
+  const [isPending, setIsPending] = useState(false);
+  const [images, setImages] = useState(movie.images);
+  const navigate = useNavigate();
+  const PUT = "PUT";
+  const { id } = useParams();
 
-    
-    
-//    const handleUpdate = () => {
-//      const updateMovie = { name, body, creator, images };
-//      fetch(`http://localhost:8000/movies/` + id, {
-//        method: PUT,
-//        headers: { "Content-Type": "application/json" },
-//        body: JSON.stringify(updateMovie),
-//      }).then(() => {
-//        console.log("Movie Updated", updateMovie);
-//        setIsPending(false);
-//        navigate("/");
-//      });
-//    };
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const updateMovie = { name, body, creator, images };
+    setIsPending(true);
+    fetch(`http://localhost:8000/movies/${id}`, {
+      method: PUT,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updateMovie),
+    }).then(() => {
+      console.log("Updated Movie", updateMovie);
+      navigate("/");
+      setIsPending(false);
+    });
+  };
 
-
-const handleUpdate = () => {
-  const updateMovie = { name, body, creator, images };
-  setIsPending(true);
-  fetch(`http://localhost:8000/movies/` + id, {
-    method: PUT,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(updateMovie),
-  }).then(() => {
-    console.log("Updatd Movie", updateMovie);
-    navigate("/");
-    setIsPending(false);
-  });
-};
-
-    return (
-            <div className="modal form">
-            <form  className="modal-content" onSubmit={handleUpdate}>
-                <input value={name} onChange={(e) =>  setName(e.target.value)} placeholder="Movie name" />
-                <textarea value={body} onChange={(e) => setBody(e.target.value)}  placeholder="Movie details"/> 
-                <select value={creator} onChange={(e)=> setCreator(e.target.value)}>
-                    <option value="mario">mario</option>
-                    <option value="Mark">mark</option>
-                </select>
-                <input  value={images} type="url" onChange={(e) => setImages(e.target.value)}/>
-               {!isPending && <button>Update</button>}
-               { isPending && <button>Updating...</button>}
-            </form>
-        </div>
-    )
+  return (
+    <div className="modal form">
+      <form className="modal-content" onSubmit={handleUpdate}>
+        <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Movie name" />
+        <textarea value={body} onChange={(e) => setBody(e.target.value)} placeholder="Movie details" />
+        <select value={creator} onChange={(e) => setCreator(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="Mark">mark</option>
+        </select>
+        <input value={images} onChange={(e) => setImages(e.target.value)} />
+        {!isPending && <button>Update</button>}
+        {isPending && <button>Updating...</button>}
+      </form>
+    </div>
+  );
 }
